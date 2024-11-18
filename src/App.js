4636e1196dcedd1209846add2992607e03bee925
mohanreddytm <tamminenimohanreddy1@gmail.com>
2024-11-18 15:50:28 +0530

    Update Mohan to deatailed manner

--- src/App.js
+++ src/App.js
@@ -5,21 +5,10 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>mohan</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <h1>mohan reddy is the real hero</h1>
+        
+
     </div>
   );
 }
